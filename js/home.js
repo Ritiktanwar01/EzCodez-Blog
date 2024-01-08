f6d5ed8f030e77e99e30db8ed62efa3569d3f71d
ritik337 <59756227+ritik337@users.noreply.github.com>
2024-01-08 17:03:28 +0530

    Uploading files

---
+++ js/home.js
@@ -0,0 +1,6 @@
+function btntoggle(){
+    let button = document.getElementById('btntoggle')
+    button.classList.toggle('togglebtn')
+    button.classList.toggle('togglebtn-2')
+    console.log('clicked')
+}
