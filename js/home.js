90703b81091f0ffdc648d4a71a7c5785d97d4805
ritik337 <59756227+ritik337@users.noreply.github.com>
2024-01-10 14:52:23 +0530

    improved home page

--- js/home.js
+++ js/home.js
@@ -4,3 +4,12 @@ function btntoggle(){
     button.classList.toggle('togglebtn-2')
     console.log('clicked')
 }
+function inptclr(){
+    document.getElementById('inputBX').value = ""
+}
+function searchRemove(){
+    document.getElementById('searchSec').style.display = 'none'
+}
+function searchShow(){
+    document.getElementById('searchSec').style.display = 'flex'
+}
